6d230de3553895d67f3d7f6897b2ccf8bf8f15ce
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2020-09-15 23:55:07 +0200

    SI fahk yuh for Marco

--- SDL-Research/Main.cpp
+++ SDL-Research/Main.cpp
@@ -2,33 +2,37 @@
 // Created by husey on 29-8-2020.
 //
 
-#include <cstdio>
-#include "SDL.h"
+#include <iostream>
+//#include "SDL.h"
+
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+//    SDL_Init(SDL_INIT_VIDEO);
+//
+//    SDL_Window *window = SDL_CreateWindow(
+//            "SDL2Test",
+//            SDL_WINDOWPOS_UNDEFINED,
+//            SDL_WINDOWPOS_UNDEFINED,
+//            640,
+//            480,
+//            0
+//    );
+//
+//    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+//    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+//    SDL_RenderClear(renderer);
+//    SDL_RenderPresent(renderer);
+//
+//    SDL_Delay(3000);
+//
+//    SDL_DestroyWindow(window);
+//    SDL_Quit();
 
-    SDL_Window *window = SDL_CreateWindow(
-            "SDL2Test",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            640,
-            480,
-            0
-    );
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(3000);
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    cout << "Fahk yuh cyka blaty" << endl;
+    cout << "FAHK YUH MARCO BITCH" << endl;
 
     return 0;
 }
