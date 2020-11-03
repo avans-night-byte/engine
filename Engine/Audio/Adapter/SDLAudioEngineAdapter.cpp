57928dfdd9fb3b49e18059152f60cad6ffbf7b77
Storm <stormwitziers123@gmail.com>
2020-11-03 22:13:50 +0100

    replaced the previous sound system with a new one

--- Engine/Audio/Adapter/SDLAudioEngineAdapter.cpp
+++ Engine/Audio/Adapter/SDLAudioEngineAdapter.cpp
@@ -2,12 +2,59 @@
 // Created by Storm on 05/10/2020.
 //
 
+#include <vector>
 #include "SDLAudioEngineAdapter.hpp"
 
-void SDLAudioEngineAdapter::getAudioFiles() {
+SDLAudioEngineAdapter::SDLAudioEngineAdapter()
+{
+
+    // TODO: Try higher frequency
+    if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+    {
+        printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+    }
+
+    if(Mix_Init(MIX_INIT_MP3) < 0)
+    {
+        printf( "Failed to init Mix: %s\n", Mix_GetError() );
+    }
+}
+
+std::vector<std::string> SDLAudioEngineAdapter::getAudioFiles() {
+    std::vector<std::string> keys;
+
+    //Get from global music
+    for(auto const& musicFile : _globalMusic)
+        keys.push_back(musicFile.first);
+    for(auto const& soundFile : _sounds)
+        keys.push_back(soundFile.first);
+
+    return keys;
+}
+
+void SDLAudioEngineAdapter::playAudioFile(const std::string& name) {
+    Mix_Music* music = _globalMusic.at(name);
+    if(music != nullptr){
+        Mix_PlayMusic(music, 1);
+        return;
+    }
+    Mix_Chunk* soundEffect = _sounds.at(name);
+    if(soundEffect != nullptr){
+        Mix_PlayChannel(-1, soundEffect, 0);
+    }
 
 }
 
-void SDLAudioEngineAdapter::playAudioFile() {
+void SDLAudioEngineAdapter::loadInMemory(const std::string& path, fileType type) {
 
+    switch (type) {
+        case music:
+            _globalMusic[path] = Mix_LoadMUS(path.c_str());
+            break;
+        case sound:
+            _sounds[path] = Mix_LoadWAV(path.c_str());
+            break;
+    }
 }
+
+
--- Engine/Audio/Adapter/SDLAudioEngineAdapter.hpp
+++ Engine/Audio/Adapter/SDLAudioEngineAdapter.hpp
@@ -4,10 +4,27 @@
 #ifndef NIGHTBYTE_SDLAUDIOENGINEADAPTER_HPP
 #define NIGHTBYTE_SDLAUDIOENGINEADAPTER_HPP
 
+#include "SDL_mixer.h"
+#include <iostream>
+#include <map>
+
 class SDLAudioEngineAdapter {
 public:
-    void getAudioFiles();
+    SDLAudioEngineAdapter();
 
-    void playAudioFile();
+    enum fileType { music, sound };
+
+    std::vector<std::string> getAudioFiles();
+    void playAudioFile(const std::string& name);
+    void loadInMemory(const std::string &path, fileType type);
+
+
+
+private:
+    //The music that will be played
+    std::map<std::string, Mix_Music*> _globalMusic;
+    std::map<std::string, Mix_Chunk*> _sounds;
+
+
 };
 #endif //NIGHTBYTE_SDLAUDIOENGINEADAPTER_HPP
