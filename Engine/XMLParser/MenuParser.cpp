44769ae6b998a171bf03f6824e2f34fed2e3e4f2
Storm <stormwitziers123@gmail.com>
2020-12-09 21:01:48 +0100

    Created game over menu

--- Engine/XMLParser/MenuParser.cpp
+++ Engine/XMLParser/MenuParser.cpp
@@ -202,10 +202,6 @@ void MenuParser::onClick(const Input &input) {
                 ResourceManager::getInstance()->loadResource(nextScene);
             }
 
-
-            if(button.events().onClick()->loadLevel().present())
-                ResourceManager::getInstance()->loadResource(button.events().onClick()->loadLevel()->c_str());
-
             if (button.events().onClick()->loadURL().present()) {
 #if WIN32
                 //Windows open
